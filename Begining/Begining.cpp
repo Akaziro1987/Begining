3235ce61b4f9174f4613643eb9981736f3265d0e
lenovo <lenovo@DESKTOP-H11UT5P>
2025-09-13 14:02:31 +0100

    Add project files.

---
+++ Begining/Begining.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+int main()
+{
+	std::cout << "Name: Nazarek\n";
+	std::cout << "Age: 18\n";
+	std::cout << "Country: Italy\n";
+	std::cout << "Hobby: codex\n";
+	return 0;
+
+}
