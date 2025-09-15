788622290ed46d04802e0190f0f26466452441fc
lenovo <lenovo@DESKTOP-H11UT5P>
2025-09-15 12:26:24 +0100

    new

--- Begining/Begining.cpp
+++ Begining/Begining.cpp
@@ -1,22 +1,12 @@
 
 #include <iostream>
-using namespace std;
 int main ()
-{
-	//this is a lesson about the difference between \n and endl
 
-	cout << "Guide?" << endl;
-	cout << "what is the difference between those functions? (\\n and endl)" << endl;
-	cout << "both are used to create a new line" << endl;
-	cout << "endl is used to flush the buffer" << endl;
-	cout << "buffer is a temporary storage area" << endl;
-	cout << "and \\n is just a new line character" << endl;
-	cout << "it does not flush the buffer" << endl;
-	cout << "but endl is slower because it flushes the buffer everytime " << endl;
-	cout << "so if you want to use endl\n";
-	cout << "use it only when you need to flush the buffer" << endl;
-	cout << "otherwise use \\n" << endl;
-	cout << "the end!" << endl;
+{
+	std::cout << "Name: Nazarek" << std::endl;
+	std::cout << "Age 20" << std::endl;
+	std::cout << "Country: Marocco\n";
+	std::cout << "City: Casablanca" << std::endl;
 
 	return 0;
 
