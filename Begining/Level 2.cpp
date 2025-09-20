bbd2416ae98d0ee31b6e5f917e6b38438b7c500c
Akaziro1987 <xcreator2goal@gmail.com>
2025-09-20 20:21:40 +0100

    Add files via upload

---
+++ Begining/Level 2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//this is a variable lesson there are many types of variables
+	//there are 3 parts = Type, identifiers,literals
+
+	cout << "\a";
+
+	int number = 1987;
+
+	float decimal = 57.78;
+
+	double bigdecimal = 57.58888888888888888;
+
+	char oneletter = 'A';
+
+	string word = "Hello";
+
+	bool trueorfalse = true;
+
+	//homework
+
+	string N = "NAZAREK";
+	int A = 19;
+	string C = "Italy";
+	string S = "Milano";
+	int salary = 5000;
+	string G = "Male";
+
+	cout << "Name: " << N << endl;
+	cout << "Age: " << A << endl;
+	cout << "Country: " << C << endl;
+	cout << "city: " << S << endl;
+	cout << "Mounthly Salary: " << salary << endl;
+	cout << "Year Salary: " << salary * 12 << endl;
+	cout << "Gender: " << G << endl;
+
+
+
+
+	return 0;
+
+}
