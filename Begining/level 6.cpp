5b5559ebfde7323edd3501853258acab3abda80b
Nazarek <xcreator2goal@gmail.com>
2025-10-03 13:12:55 +0100

    Add files via upload

---
+++ Begining/level 6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+
+{
+	short num1, num2;
+	
+	cout << "plz enter number 1" << endl;
+	cin >> num1;
+	cout << " plz enter number 2" << endl; 
+	cin >> num2;
+
+
+	cout << (num1 == num2) << endl;
+	cout << (num1 != num2) << endl;
+	cout << (num1 < num2) << endl;
+	cout << (num1 > num2) << endl;
+	
+
+
+		return 0;
+}
